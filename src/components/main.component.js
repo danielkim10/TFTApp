17b56385809c7a888e17daa6954b1ab9926200b5
unknown <daniel.k.0710@gmail.com>
2019-10-29 23:18:10 -0400

    Some randomization code

--- src/components/main.component.js
+++ src/components/main.component.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Button, Card, CardBody, Col, Row} from 'reactstrap';
 import axios from 'axios';
 import Hexagon from 'react-hexagon';
+import HexagonGrid from 'react-hexagon-grid';
 
 export default class Main extends Component {
   constructor(props) {
@@ -17,8 +18,14 @@ export default class Main extends Component {
       origins: [],
       activeClasses: [],
       activeOrigins: [],
+      t1Champs: [],
+      t2Champs: [],
+      t3Champs: [],
+      t4Champs: [],
+      t5Champs: [],
     }
     this.clearButton = this.clearButton.bind(this);
+    this.fillArrays = this.fillArrays.bind(this);
     this.randomButton = this.randomButton.bind(this);
   }
 
@@ -57,6 +64,18 @@ export default class Main extends Component {
             });
   }
 
+  fillArrays() {
+    if (this.state.t1Champs.length == 0) {
+      let t1Champs = (this.state.champions.filter(champion => champion.cost[0] == 1));
+      let t2Champs = (this.state.champions.filter(champion => champion.cost[0] == 2));
+      let t3Champs = (this.state.champions.filter(champion => champion.cost[0] == 3));
+      let t4Champs = (this.state.champions.filter(champion => champion.cost[0] == 4));
+      let t5Champs = (this.state.champions.filter(champion => champion.cost[0] == 5));
+
+      this.setState({t1Champs: t1Champs, t2Champs: t2Champs, t3Champs: t3Champs, t4Champs: t4Champs, t5Champs: t5Champs});
+    }
+  }
+
   addToTeam() {
 
   }
@@ -78,6 +97,65 @@ export default class Main extends Component {
   }
 
   randomButton() {
+    this.fillArrays();
+    const T1_CHAMPS = 4;
+    const T2_CHAMPS = 5;
+    const T3_CHAMPS = 4;
+    const T4_CHAMPS = 3;
+    const T5_CHAMPS = 3;
+    const MAX_CHAMPS = 9;
+    const MAX_3_AND_UNDER = 7; // roll between 5 and 7 low tier
+    const MAX_4_AND_UP = 4; // roll between 2 and 4 high tier
+    let t5, t4, t3, t2, t1 = 0;
+
+    let lowTierChamps = Math.floor(Math.random() * 3);
+    let highTierChamps = Math.floor(Math.random() * 2);
+
+    t5 = Math.floor(Math.random() * T5_CHAMPS);
+    highTierChamps -= t5;
+    t4 = highTierChamps;
+
+    t3 = Math.floor(Math.random() * T3_CHAMPS);
+    lowTierChamps -= t3;
+    if (lowTierChamps != 0) {
+      t2 = Math.floor(Math.random() * lowTierChamps);
+      lowTierChamps -= t2;
+      t1 = lowTierChamps;
+    }
+    else {
+      t2 = 0;
+      t1 = 0;
+    }
+    let t5Champs = this.state.t5Champs;
+    let t4Champs = this.state.t4Champs;
+    let t3Champs = this.state.t3Champs;
+    let t2Champs = this.state.t2Champs;
+    let t1Champs = this.state.t1Champs;
+    for (let i = 0; i < t5; i++) {
+      team.push(t5Champs[Math.floor(Math.random() * t5Champs.length)]);
+    }
+    for (let i = 0; i < t4; i++) {
+      team.push(t4Champs[Math.floor(Math.random() * t4Champs.length)]);
+    }
+    for (let i = 0; i < t3; i++) {
+      team.push(t3Champs[Math.floor(Math.random() * t3Champs.length)]);
+    }
+    for (let i = 0; i < t2; i++) {
+      team.push(t2Champs[Math.floor(Math.random() * t2Champs.length)]);
+    }
+    for (let i = 0; i < t1; i++) {
+      team.push(t1Champs[Math.floor(Math.random() * t1Champs.length)]);
+    }
+
+
+    //let t1 = Math.floor(Math.random() * t1Champs);
+    //let t2 = Math.floor(Math.random() * t2Champs);
+    //let t3 = Math.floor(Math.random() * t3Champs);
+    //let t4 = Math.floor(Math.random() * t4Champs);
+    //let t5 = Math.floor(Math.random() * t5Champs);
+
+
+
     console.log("button clicked");
   }
 
@@ -110,9 +188,11 @@ export default class Main extends Component {
 
             </CardBody>
           </Card>
-          <Card style={{width: "100%"}} name="board">
+          <Card>
             <CardBody>
-              <Hexagon diagonal="0.5"/><Hexagon diagonal="0.5"/><Hexagon diagonal="0.5"/><Hexagon diagonal="0.5"/><Hexagon diagonal="0.5"/><Hexagon diagonal="0.5"/><Hexagon diagonal="0.5"/>
+              <HexagonGrid gridWidth={700} gridHeight={600}/>
+              <Row><svg height="100px" width="100px"><Hexagon flatTop/></svg><svg height="100px" width="100px"><Hexagon flatTop/></svg><svg height="100px" width="100px"><Hexagon flatTop/></svg><svg height="100px" width="100px"><Hexagon flatTop/></svg><svg height="100px" width="100px"><Hexagon flatTop/></svg><svg height="100px" width="100px"><Hexagon flatTop/></svg><svg height="100px" width="100px"><Hexagon flatTop/></svg></Row>
+              <Row><svg height="100px" width="100px"><Hexagon flatTop/></svg><svg height="100px" width="100px"><Hexagon flatTop/></svg><svg height="100px" width="100px"><Hexagon flatTop/></svg><svg height="100px" width="100px"><Hexagon flatTop/></svg><svg height="100px" width="100px"><Hexagon flatTop/></svg><svg height="100px" width="100px"><Hexagon flatTop/></svg><svg height="100px" width="100px"><Hexagon flatTop/></svg></Row>
             </CardBody>
           </Card>
           <Card name="pool">
